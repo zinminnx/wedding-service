f40c1a137edcb9a4883211ec36b023d9248cf206
zinminnx <zinminn.htet.mm@gmail.com>
2026-09-16 00:37:43 +0630

    fix: configure production static root

--- config/settings.py
+++ config/settings.py
@@ -166,7 +166,8 @@ USE_TZ = True
 # Static files
 # --------------------------------------------------
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # --------------------------------------------------
